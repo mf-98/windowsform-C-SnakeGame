8840049f9fd903fb94ac44b4ef4756f0f899488a
mf-98 <61858117+mf-98@users.noreply.github.com>
2020-03-12 22:40:50 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Game
+{
+    partial class GameBoard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(GameBoard));
+            this.sprites = new System.Windows.Forms.ImageList(this.components);
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.gameScreen = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.gameScreen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // sprites
+            // 
+            this.sprites.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("sprites.ImageStream")));
+            this.sprites.TransparentColor = System.Drawing.Color.Transparent;
+            this.sprites.Images.SetKeyName(0, "Retro-Block-icon.png");
+            this.sprites.Images.SetKeyName(1, "images.png");
+            this.sprites.Images.SetKeyName(2, "NSLU_Snake_Block_unused.png");
+            this.sprites.Images.SetKeyName(3, "unnamed (2).png");
+            this.sprites.Images.SetKeyName(4, "images.jpg");
+            this.sprites.Images.SetKeyName(5, "cf4b85ba4900ee1d74a020abc297e5e6.png");
+            this.sprites.Images.SetKeyName(6, "tile_brick.png");
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 250;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // gameScreen
+            // 
+            this.gameScreen.BackColor = System.Drawing.Color.LimeGreen;
+            this.gameScreen.Location = new System.Drawing.Point(25, 6);
+            this.gameScreen.Name = "gameScreen";
+            this.gameScreen.Size = new System.Drawing.Size(420, 420);
+            this.gameScreen.TabIndex = 0;
+            this.gameScreen.TabStop = false;
+            // 
+            // GameBoard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 438);
+            this.Controls.Add(this.gameScreen);
+            this.Name = "GameBoard";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Welcome!";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.snake_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.gameScreen)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ImageList sprites;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox gameScreen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class GameBoard : Form
+    {
+        
+        struct SnakePosition
+        {
+            public int XPOS;
+            public int YPOS;
+        }
+
+        enum TileType
+        {
+            EMPTY,
+            SNAKE,
+            TREAT
+        };
+
+        enum Directions
+        {
+            Up,
+            Down,
+            Left,
+            Right
+        };
+        
+
+        SnakePosition[] snakeArray;
+        Random rand;
+        int bodyLength; 
+        Directions movementDir;
+        TileType[,] tileArray;
+        Graphics graphics;
+
+        public GameBoard()
+        {
+            InitializeComponent();
+            tileArray = new TileType[11, 11]; 
+            snakeArray = new SnakePosition[100]; 
+            rand = new Random();
+        }
+
+        private void Game_Load(object sender, EventArgs e)
+        {
+            gameScreen.Image = new Bitmap(420, 420); 
+            graphics = Graphics.FromImage(gameScreen.Image);
+            graphics.Clear(Color.White);
+
+            for (int i = 1; i <= 10; i++)
+            {
+                
+                graphics.DrawImage(sprites.Images[6], i * 35, 0);
+                graphics.DrawImage(sprites.Images[6], i * 35, 385); 
+            }
+
+            for (int i = 0; i <= 11; i++)
+            {
+                
+                graphics.DrawImage(sprites.Images[6], 0, i * 35); 
+                graphics.DrawImage(sprites.Images[6], 385, i * 35); 
+            }
+
+            
+            snakeArray[0].XPOS = 5; 
+            snakeArray[0].YPOS = 5;
+            snakeArray[1].XPOS = 5;
+            snakeArray[1].YPOS = 6;
+            snakeArray[2].XPOS = 5;
+            snakeArray[2].YPOS = 7;
+
+            graphics.DrawImage(sprites.Images[5], 5 * 35, 5 * 35); 
+            graphics.DrawImage(sprites.Images[4], 5 * 35, 6 * 35); 
+            graphics.DrawImage(sprites.Images[4], 5 * 35, 7 * 35); 
+
+            tileArray[5, 5] = TileType.SNAKE;
+            tileArray[5, 6] = TileType.SNAKE; 
+            tileArray[5, 7] = TileType.SNAKE; 
+
+            movementDir = Directions.Up;
+            bodyLength = 3;
+
+            for (int i = 0; i < 4; i++)
+            {
+                Treat();
+            }
+        }
+
+        private void Treat()
+        {
+            var imageIndex = rand.Next(0, 4);
+            int x;
+            int y;
+            
+
+            do
+            {
+                x = rand.Next(1, 10);
+                y = rand.Next(1, 10);
+            }
+            while (tileArray[x, y] != TileType.EMPTY);
+
+            tileArray[x, y] = TileType.TREAT; 
+            graphics.DrawImage(sprites.Images[imageIndex], x * 35, y * 35); 
+        }
+
+       
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            graphics.FillRectangle(Brushes.White, snakeArray[bodyLength - 1].XPOS * 35,
+                snakeArray[bodyLength - 1].YPOS * 35, 35, 35);
+
+            tileArray[snakeArray[bodyLength - 1].XPOS, snakeArray[bodyLength - 1].YPOS] = TileType.EMPTY;
+
+
+            for (int i = bodyLength; i >= 1; i--)
+            {
+                snakeArray[i].XPOS = snakeArray[i - 1].XPOS;
+                snakeArray[i].YPOS = snakeArray[i - 1].YPOS;
+            }
+
+            graphics.DrawImage(sprites.Images[4], snakeArray[0].XPOS * 35, snakeArray[0].YPOS * 35);
+
+
+            switch (movementDir)
+            {
+                case Directions.Up:
+                    snakeArray[0].YPOS = snakeArray[0].YPOS - 1;
+                    break;
+                case Directions.Down:
+                    snakeArray[0].YPOS = snakeArray[0].YPOS + 1;
+                    break;
+                case Directions.Left:
+                    snakeArray[0].XPOS = snakeArray[0].XPOS - 1;
+                    break;
+                case Directions.Right:
+                    snakeArray[0].XPOS = snakeArray[0].XPOS + 1;
+                    break;
+            }
+
+
+            if (snakeArray[0].XPOS < 1 || snakeArray[0].XPOS > 10 || snakeArray[0].YPOS < 1 || snakeArray[0].YPOS > 10)
+            {
+                GameOver();
+                gameScreen.Refresh();
+                return;
+            }
+
+
+            if (tileArray[snakeArray[0].XPOS, snakeArray[0].YPOS] == TileType.SNAKE)
+            {
+                GameOver();
+                gameScreen.Refresh();
+                return;
+            }
+
+
+            if (tileArray[snakeArray[0].XPOS, snakeArray[0].YPOS] == TileType.TREAT)
+            {
+
+                graphics.DrawImage(sprites.Images[4], snakeArray[bodyLength].XPOS * 35,
+                    snakeArray[bodyLength].YPOS * 35);
+
+                tileArray[snakeArray[bodyLength].XPOS, snakeArray[bodyLength].YPOS] = TileType.SNAKE;
+                bodyLength++;
+
+
+                if (bodyLength < 96)
+                    Treat();
+
+                this.Text = "Snake - score: " + bodyLength;
+            }
+
+
+            graphics.DrawImage(sprites.Images[5], snakeArray[0].XPOS * 35, snakeArray[0].YPOS * 35);
+
+            tileArray[snakeArray[0].XPOS, snakeArray[0].YPOS] = TileType.SNAKE;
+
+
+            gameScreen.Refresh();
+        }
+
+        private void GameOver()
+        {
+            timer1.Enabled = false;
+            MessageBox.Show("YOU LOST", "Game Over", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+        }
+
+        private void snake_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Up:
+                    movementDir = Directions.Up;
+                    break;
+                case Keys.Down:
+                    movementDir = Directions.Down;
+                    break;
+                case Keys.Left:
+                    movementDir = Directions.Left;
+                    break;
+                case Keys.Right:
+                    movementDir = Directions.Right;
+                    break;
+            }
+        }
+    }
+}
